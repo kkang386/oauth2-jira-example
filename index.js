25c35ba0d832624f68e82658642c0b5e16dd14af
Kenny Kang <kenny.kang@workboard.com>
2021-09-29 18:39:00 -0700

    got more done with additional service call. Refresh token still not working

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
 require('dotenv').config();
+// import express from 'express';
+// import sprightly from 'sprightly';
+const sprightly = require("sprightly");
 const axios = require('axios');
+const { query } = require('express');
 const express = require('express');
 const path = require('path');
 const app = express();
-
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser')
 app.use(express.static('static'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine('spy', sprightly);
+app.set('views', './static/');       // specify the views directory (its ./views by default)
+app.set('view engine', 'spy'); // register the template engine
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/static/index.html'));
 });
 
-app.get('/done', (req, res) => {
-    res.sendFile(path.join(__dirname, '/static/done.html'));
-  });
-  
-
 app.get('/auth', (req, res) => {
+    var action = req.query.action;
+    res.cookie('action', action, { maxAge: 900000, httpOnly: true });
+    
+    var scope= "";
     console.log("\n in /auth, starting get code");
+    console.log('action:' + action);
+    switch(action) {
+        case "user_id_api":
+            scope = "read:me read:account";
+            break;
+        case "confluence_api":
+            scope = "read:confluence-content.summary write:confluence-content read:confluence-space.summary write:confluence-space write:confluence-file read:confluence-props manage:confluence-configuration read:confluence-content.all write:confluence-props search:confluence read:confluence-content.permission read:confluence-groups write:confluence-groups readonly:content.attachment:confluence read:confluence-user";
+        case "jira_svc_desk_api":
+            scope = "read:servicedesk-request manage:servicedesk-customer write:servicedesk-request read:servicemanagement-insight-objects";
+            break;
+        case "jira_platform_rest_api":
+            scope = "read:jira-user read:jira-work write:jira-work";
+    }
     var url2="https://auth.atlassian.com/authorize?audience=api.atlassian.com&client_id="+ 
         process.env.CLIENT_ID +
-        "&scope=read%3Ame%20read%3Aaccount&redirect_uri=" +
-        encodeURIComponent(process.env.HOST_NAME) + "/oauth-callback&state=" + 
-        encodeURIComponent(process.env.MY_BOUND_VALUE) +
+        "&scope="+ encodeURIComponent(scope) +
+        "&redirect_uri=" + encodeURIComponent(process.env.HOST_NAME) + "/oauth-callback" + 
+        "&state=" +  encodeURIComponent(process.env.MY_BOUND_VALUE) +
         "&response_type=code&prompt=consent"
         console.log("url2: " + url2);
     res.redirect(
@@ -69,16 +92,111 @@ app.get('/accessible-resources', ({ query: { token } }, res) => {
     console.log(opts);
     
     axios
-      .get(url, opts)
-      .then((response) => {
-        console.log("response.data: ");
-        console.log(response.data);
-  
-        res.redirect('/done');
-      })
-      .catch((err) => res.status(500).json({ err: err.message }));
+        .get(url, opts)
+        .then((response) => {
+            console.log("response.data: ");
+            console.log(response.data);
+            res.render('done.spy', {data: JSON.stringify(response.data), token: token});
+        })
+        .catch((err) => res.status(500).json({ err: err.message }));
   
 });
+
+app.post('/appstart', (req, res) => {
+    var action = req.cookies['action'];
+    const token = req.body.token;
+    const opts = { headers: { accept: 'application/json',  'Authorization': 'Bearer ' + token} };
+    const payload = JSON.parse(req.body.payload);
+    const submit_value = req.body.submit;
+    if (submit_value == "Refresh token") {
+        action = 'refresh-token';
+    }
+
+    console.log("action: " + action);
+    console.log("req.body.payload ");
+    console.log(payload);
+    var cloud_id = '';
+    var server = '';
+    payload.forEach((entry) => {
+        if (entry.name == process.env.ATLASSIAN_SITE_NAME) {
+            cloud_id = entry.id;
+            server = entry.url;
+        }
+    });
+    var url = method = '';
+    switch (action) {
+        case 'user_id_api':
+            url = 'https://api.atlassian.com/me';
+            axios
+                .get(url, opts)
+                .then((response) => {
+                    console.log("response.data: ");
+                    console.log(response.data);
+                    res.render('view.spy', {data: JSON.stringify(response.data)});
+                })
+                .catch((err) => res.status(500).json({ err: err.message }));
+            break;
+        case "confluence_api":
+            // only testing confluence space api 
+            url = 'https://api.atlassian.com/ex/confluence/'+ cloud_id + '/rest/api/space';
+
+            // code below is untested
+            axios
+                .get(url, opts)
+                .then((response) => {
+                    console.log("response.data: ");
+                    console.log(response.data);
+                    res.render('view.spy', {data: JSON.stringify(response.data)});
+                })
+                .catch((err) => res.status(500).json({ err: err.message }));
+
+        case "jira_svc_desk_api":
+            scope = "read:servicedesk-request manage:servicedesk-customer write:servicedesk-request read:servicemanagement-insight-objects";
+            console.log("jira_svc_desk_api is not covered.");
+            res.sendFile(path.join(__dirname, '/static/done.html'));
+            break;
+        case "jira_platform_rest_api":
+            url = 'https://api.atlassian.com/ex/jira/'+ cloud_id + '/rest/api/2/project';
+
+            axios
+                .get(url, opts)
+                .then((response) => {
+                    console.log("\n in /appstart, GET Jira API results:");
+                    console.log("response.data: ");
+                    console.log(response.data);
+                    res.render('view.spy', {data: JSON.stringify(response.data)});
+                })
+                .catch((err) => res.status(500).json({ err: err.message }));
+            break;
+        case 'refresh-token':
+            const body = {
+                'grant_type': 'refresh_token',
+                'client_id': process.env.CLIENT_ID,
+                'client_secret': process.env.CLIENT_SECRET,
+                'refresh_token': token
+            };
+            const opts2 = { headers: { accept: 'application/json', 'Content-Type': 'application/json' } };
+            console.log("body:");
+            console.log(body);
+            
+            axios
+                .post('https://auth.atlassian.com/oauth/token', body, opts2)
+                .then((_res) => {
+                    var access_token = _res.data.access_token
+                    var refresh_token = _res.data.refresh_token
+                    console.log("\n in /appstart, GET Refresh Token");
+                    console.log("response data: ");
+                    console.log(_res.data);
+                    
+                    res.redirect(`/accessible-resources?token=${refresh_token}`);
+                })
+                .catch((err) => res.status(500).json({ err: err.message }));
+
+    }
+    //res.sendFile(path.join(__dirname, '/static/done.html'));
+});
+  
+
 
 app.listen(3000);
 // eslint-disable-next-line no-console
